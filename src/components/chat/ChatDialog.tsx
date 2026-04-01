40990fbfa2609fc6c6bd07fd28066aab1552cadc
Marko Petronijević <markopetronijevic666@gmail.com>
2026-04-01 17:57:56 +0200

    Update ChatDialog.tsx

--- src/components/chat/ChatDialog.tsx
+++ src/components/chat/ChatDialog.tsx
@@ -1327,9 +1327,9 @@ const devPanelPreview = {
           </div>
         )}
 
-        {/* Main chat area */}
-        <div {!devMode && (
-  <div className="flex flex-col min-w-0 relative flex-1">className={cn("flex flex-col min-w-0 relative", devMode ? "w-[35%] shrink-0" : "flex-1")}>
+       {/* Main chat area */}
+{!devMode && (
+  <div className="flex flex-col min-w-0 relative flex-1">
           {/* Header */}
           <div className="flex items-center justify-between px-4 py-2.5 border-b border-white/[0.06]">
             <div className="flex items-center gap-2">
