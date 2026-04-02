69eacd79981b85d50e0930af4614da6c70549452
Marko Petronijević <markopetronijevic666@gmail.com>
2026-04-02 19:11:59 +0200

    Fix formatting issues in ChatMessage.tsx

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -5,7 +5,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-.
+
 interface CodeBlock { language: string; code: string; label: string; }
 
 interface ChatMessageProps {
